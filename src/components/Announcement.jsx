8ed454e2fe849f47443d4fc04063ae42a21a12b4
Shreya Agrawal <shreya.agrawal@ihsmarkit.com>
2022-04-18 18:09:09 +0530

    my first commit reactwesbite

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,20 @@
+// In the top most section of the website
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>Get to know the best medical Facilities here</Container>;
+};
+
+export default Announcement;
